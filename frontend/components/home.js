fc961cd0c3345f86d0f9df8bb3c213ae7df77056
Nicole Hemenway <nkhemenway@gmail.com>
2017-04-15 05:05:02 -0700

    make search bar aware of input change

--- frontend/components/home.js
+++ frontend/components/home.js
@@ -6,6 +6,14 @@ import SearchBar from './search_bar.js';
 import Header from './header';
 
 class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.businessSearch = this.businessSearch.bind(this);
+  }
+
+  businessSearch(term){
+    console.log(term);
+  }
 
   render() {
     return (
@@ -14,7 +22,7 @@ class Home extends React.Component {
           loggedIn={this.props.loggedIn}
           logout={ this.props.logout } />
         <h1>gulp</h1>
-        <SearchBar />
+        <SearchBar onSearchTermChange={ this.businessSearch }/>
       </div>
     );
   }
@@ -35,3 +43,62 @@ const mapDispatchToProps = (dispatch, state) => {
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Home);
+
+
+
+// import _ from 'lodash';
+// import React, { Component } from 'react';
+// import ReactDOM from 'react-dom';
+// import YTSearch from 'youtube-api-search';
+//
+// import SearchBar from './components/search_bar';
+// import VideoList from './components/video_list';
+// import VideoDetail from './components/video_detail';
+//
+// const API_KEY = 'AIzaSyD3UxNbOVMcjlHUPBD37eybsFYWFJGVJg0';
+//
+//
+// class App extends Component {
+//   constructor(props){
+//     super(props);
+//
+//     this.state = {
+//       videos: [],
+//       selectedVideo: null
+//     };
+//
+//     this.videoSearch('puppets');
+//
+//   }
+//
+//   videoSearch(searchTerm){
+//     YTSearch(
+//       {key: API_KEY, term: searchTerm},
+//       (videos) => {
+//         this.setState({
+//           videos: videos,
+//           selectedVideo: videos[0]
+//         });
+//        }
+//     );
+//   }
+//
+//   render() {
+//     const videoSearch = _.debounce( searchTerm => {
+//       this.videoSearch(searchTerm);
+//     }, 300);
+//
+//     return (
+//       <div>
+//         <SearchBar onSearchTermChange={videoSearch}/>
+//         <VideoDetail video={this.state.selectedVideo}/>
+//         <VideoList
+//           onVideoSelect={ selectedVideo => {
+//             this.setState({selectedVideo});
+//           }}
+//           videos={this.state.videos}
+//         />
+//       </div>
+//     );
+//   }
+// }
--- frontend/components/search_bar.js
+++ frontend/components/search_bar.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class SearchBar extends React.Component {
+class SearchBar extends Component {
+  constructor(props){
+    super(props);
+    this.state = { term: '' };
+  }
 
-  render() {
+  render(){
     return (
-      <div>
-        <p>searchbar</p>
+      <div className='search-bar'>
+        <input
+          placeholder="bubble tea, wineries, Philz"
+          value={this.state.term}
+          onChange={ event => {
+            this.onInputChange(event.target.value);
+          }}
+        />
       </div>
     );
   }
 
+  onInputChange(searchTerm){
+    this.setState({term: searchTerm});
+    this.props.onSearchTermChange(searchTerm);
+  }
 }
 
 export default SearchBar;
