6893389609c002d4b900a0a6867e4f2a5b6ab787
Nicole Hemenway <nkhemenway@gmail.com>
2017-04-15 15:13:56 -0700

    seed Users with randomly generated names

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,40 @@
+User.destroy_all
+
+User.create!({f_name: 'guest', l_name: 'user', email: 'guest@email.com', password: 'guessed password'})
+
+# 'namey' gem generates random :common or :rare names based on US Census Data
+@namey = Namey::Generator.new
+
+# generate 30 random first and last names
+random_first_names = []
+random_last_names = []
+
+30.times do
+  name_types = [:common, :rare]
+
+  first_name = @namey.name(name_types.sample).split.first
+  last_name = @namey.name(name_types.sample).split.last
+
+  # ensures unique name combinations
+  while random_last_names.include?(last_name)
+    last_name = @namey.name(name_types.sample).split.last
+  end
+
+  random_first_names.push(first_name)
+  random_last_names.push(last_name)
+end
+
+# turn those 30 randomly generated names into users
+30.times do |i|
+  User.create!({
+    f_name: random_first_names[i],
+    l_name: random_last_names[i],
+    email: "#{random_first_names[i].downcase}_#{random_last_names[i].downcase}@email.com",
+    password: "#{random_last_names[i].downcase}_password"
+  })
+end
+
+
 Category.destroy_all
 
 all_categories = [
@@ -21,6 +58,3 @@ all_categories = [
 all_categories.each do |category|
   Category.create!({ name: category })
 end
-
-User.destroy_all
-User.create!({f_name: 'Nicole', l_name: 'Hemenway', email: 'nkhemenway@gmail.com', password: 'asdfasdf'})
