875717269e0d2359fb1ef8d73fa36957b27a2ad4
Nicole Hemenway <nkhemenway@gmail.com>
2017-04-16 06:01:41 -0700

    break everything while trying to sort search results by best matches

--- frontend/components/main.js
+++ frontend/components/main.js
@@ -14,22 +14,77 @@ class Main extends React.Component {
   }
 
   fetchMatches(term){
-    let results = [];
-    
+    const results = [];
+
     this.props.fetchCategories(term)
       .then( res => {
-        results.push(_.map(res.categories, cat => cat.title));
+        res.categories.forEach( cat => results.concat([cat.title]));
       });
-      console.log(results);
 
     this.props.fetchBusinesses(term)
-    .then( businesses => {
-      console.log(businesses);
+    .then( res => {
+      res.businesses.forEach( biz => results.concat([biz.name]));
     });
+
+    results.forEach(res => {console.log(res);});
+    this.bestResults(results, term);
   }
 
-  bestResults(results){
+  bestResults(results, term){
+    const bestResults = [];
+    const exactResults = [];
+    const exactMatch = word => {
+      return word === term.toLowerCase().replace(/[^a-z]/g, "");
+    };
+    console.log(results);
+    results.forEach( result => {
+      if (_.map(result.split(" "), word => word.toLowerCase().replace(/[^a-z]/g, "")).some(exactMatch)) {
 
+        exactResults.push(result);
+      }
+    });
+    // include all results that have a whole word that matches term exactly
+    bestResults.push(this.exactResults(results, term));
+
+    // up to 9, include results that have matches to starts of words
+    while (bestResults.length < 9){
+      bestResults.push(this.goodResults(results, term, bestResults));
+    }
+    // console.log("this.goodResults(results, term, bestResults):");
+    // console.log(this.goodResults(results, term, bestResults));
+
+    // console.log(bestResults);
+    // up to 6, include other matches
+
+  }
+
+  exactResults(results, term){
+    const exactResults = [];
+    const exactMatch = word => word.toLowerCase() === term;
+    results.forEach( result => {
+      if (result.split(" ").toLowerCase().some(exactMatch)) {
+        exactResults.push(result);
+      }
+    });
+
+    return exactResults;
+  }
+
+  goodResults(results, term, bestResults){
+    const goodResults = [];
+    results.forEach( result => {
+      result.split(" ").forEach( word => {
+        const isGoodMatch = Boolean(
+          word.toLowerCase().match(term).index === 0
+        );
+        const isUniqMatch = !bestResults.includes(result);
+
+        if (isGoodMatch && isUniqMatch){
+          goodResults.push(result);
+        }
+      });
+    });
+    return goodResults;
   }
 
   render() {
