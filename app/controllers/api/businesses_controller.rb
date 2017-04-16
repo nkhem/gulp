fbca923259d11f615b213aec1a52ddc400357cdc
Nicole Hemenway <nkhemenway@gmail.com>
2017-04-15 22:49:31 -0700

    seed more businessesCatergories

--- app/controllers/api/businesses_controller.rb
+++ app/controllers/api/businesses_controller.rb
@@ -1,10 +1,16 @@
 class Api::BusinessesController < ApplicationController
   def index
-    @businesses = Business.all
+    @businesses = Business.all.limit(3)
   end
 
   def show
     @business = Business.find(params[:id])
     render :show
+  end
+
+  private
+
+  def business_params
+    params.require()
   end
 end
