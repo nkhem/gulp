d309173b3944dc139361991e5008cf22ccdfa9e3
Nicole Hemenway <nkhemenway@gmail.com>
2017-04-26 10:53:49 -0700

    add Review#index

--- app/controllers/api/reviews_controller.rb
+++ app/controllers/api/reviews_controller.rb
@@ -1,6 +1,7 @@
 class Api::ReviewsController < ApplicationController
   def index
-    
+    @reviews = Review.joins(:businesses).where('businesses.id = ?', params[:businessId])
+    render :index
   end
 
   def create
