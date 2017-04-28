beeb7b275a645e9c1952a977d9224a928710fb16
Nicole Hemenway <nkhemenway@gmail.com>
2017-04-27 18:52:33 -0700

    write Api::ReviewsController#create

--- app/controllers/api/reviews_controller.rb
+++ app/controllers/api/reviews_controller.rb
@@ -1,10 +1,25 @@
 class Api::ReviewsController < ApplicationController
   def index
-    @reviews =  Review.joins(:business).where('reviews.business_id = ?', params[:businessId])
+    @reviews = Review.joins(:business).where('reviews.business_id = ?', params[:businessId])
     render :index
   end
 
   def create
+    @review = Review.new(
+      business_id: params[:businessId],
+      user_id: params[:userId],
+      content: params[:content],
+      rating: params[:rating]
+    )
+
+    if @review.save
+      render "api/businesses/#{params[:businessId]}"
+    else
+      render(
+        json: ['Invalid credentials'],
+        status: 401
+      )
+    end
   end
 
   def destroy
