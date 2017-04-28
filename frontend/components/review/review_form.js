e28405f9794d54cb25947d139794aae4cc41d815
Nicole Hemenway <nkhemenway@gmail.com>
2017-04-27 18:31:23 -0700

    disable review submission form if !loggedIn

--- frontend/components/review/review_form.js
+++ frontend/components/review/review_form.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 class ReviewForm extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {
       businessId: null,
       userId: null,
       content: '',
       rating: null
     };
+
+    this.renderSubmitBtn = this.renderSubmitBtn.bind(this);
   }
 
   componentWillMount(){
@@ -19,40 +23,56 @@ class ReviewForm extends React.Component {
     });
   }
 
+  renderSubmitBtn(isLoggedIn){
+    if (isLoggedIn) {
+      return <input type="submit" value='Submit Review' />;
+    } else {
+      return (
+        <div>
+          Wanna write a review? {<Link to="login">Log in</Link>} or {<Link to="signup">sign up</Link>}
+        </div>
+      );
+    }
+  }
+
   render() {
-    // console.log(this.props);
-    // console.log('ReviewForm, this.state:', this.state);
+    let isLoggedIn = this.props.currentUser;
     return (
       <div>
         <form id="new-review-form">
 
           <input
             type='text'
-            placeholder={`current userId: ${this.state.userId}`} />
+            placeholder={`current userId: ${this.state.userId}`}
+            disabled={isLoggedIn ? false : 'disabled'} />
 
           <br/>
 
           <input
             type='text'
-            placeholder={`current bizId: ${this.state.businessId}`} />
+            placeholder={`current bizId: ${this.state.businessId}`}
+            disabled={isLoggedIn ? false : 'disabled'} />
 
           <br/>
 
           <input
             type='text'
-            placeholder='rating' />
+            placeholder='rating'
+            disabled={isLoggedIn ? false : 'disabled'} />
 
           <br/>
 
           <input type='text'
-            placeholder='content' />
+            placeholder='content'
+            disabled={isLoggedIn ? false : 'disabled'} />
+
+          {this.renderSubmitBtn(isLoggedIn)}
 
           <br/>
-
-          <input type="submit" value="Submit Review" />
         </form>
       </div>
     );
+
   }
 
 }
