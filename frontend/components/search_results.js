aa7522c0eda63fff0b8bb8eb8a73c1cd6bf0e609
Nicole Hemenway <nkhemenway@gmail.com>
2017-05-01 09:19:07 -0700

    make SearchResutls functional

--- frontend/components/search_results.js
+++ frontend/components/search_results.js
@@ -2,17 +2,11 @@ import React from 'react';
 import BusinessIndex from './biz/business_index';
 import BusinessMap from './map/business_map';
 
-class SearchResults extends React.Component {
-
-  render() {
-    return (
-      <div>
-        <BusinessIndex />
-        <BusinessMap />
-      </div>
-    );
-  }
-
-}
+const SearchResults = props => (
+  <div>
+    <BusinessIndex />
+    <BusinessMap />
+  </div>
+);
 
 export default SearchResults;
