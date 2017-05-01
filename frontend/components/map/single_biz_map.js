a1fe243d2ae8b44025bd39fa9d8255dea0dd6658
Nicole Hemenway <nkhemenway@gmail.com>
2017-05-01 11:44:20 -0700

    asdfasdf

--- frontend/components/map/single_biz_map.js
+++ frontend/components/map/single_biz_map.js
@@ -4,75 +4,18 @@ import { withRouter } from 'react-router';
 
 import MarkerManager from './marker_manager';
 
-// const _getCoordsObj = latLng => ({
-//   lat: latLng.lat(),
-//   lng: latLng.lng()
-// });
-
-let _mapOptions = {
-  center: {lat: 37.773972, lng: -122.431297}, // San Francisco coords
-  zoom: 13
-};
-
 class SingleBizMap extends React.Component {
 
-    componentDidMount() {
-      this.map = new google.maps.Map(document.getElementById('single-biz-map'), _mapOptions);
-      this.MarkerManager = new MarkerManager(this.map);
-      this.MarkerManager.addMarker(this.props.business);
+  componentDidMount() {
+    const _mapOptions = {
+      center: {lat: this.props.business.lat, lng: this.props.business.lng},
+      zoom: 15
+    };
 
-      // if (this.props.singleBench) {
-      //   this.props.fetchBench(this.props.benchId);
-      // } else {
-      //   this._registerListeners();
-      //   this.MarkerManager.updateMarkers(this.props.benches);
-      // }
-    }
-
-  // componentDidMount() {
-  //   const map = this.refs.map;
-  //   this.map = new google.maps.Map(map, _mapOptions);
-  //   this.MarkerManager = new MarkerManager(this.map, this._handleMarkerClick.bind(this));
-  //   if (this.props.singleBench) {
-  //     this.props.fetchBench(this.props.benchId);
-  //   } else {
-  //     this._registerListeners();
-  //     this.MarkerManager.updateMarkers(this.props.benches);
-  //   }
-  // }
-  //
-  // componentDidUpdate() {
-  //   if(this.props.singleBench){
-  //     this.MarkerManager.updateMarkers([this.props.benches[Object.keys(this.props.benches)[0]]]); //grabs only that one bench
-  //   } else {
-  //     this.MarkerManager.updateMarkers(this.props.benches);
-  //   }
-  // }
-  //
-  // _registerListeners() {
-  //   google.maps.event.addListener(this.map, 'idle', () => {
-  //     const { north, south, east, west } = this.map.getBounds().toJSON();
-  //     const bounds = {
-  //       northEast: { lat:north, lng: east },
-  //       southWest: { lat: south, lng: west } };
-  //     this.props.updateFilter('bounds', bounds);
-  //   });
-  //   google.maps.event.addListener(this.map, 'click', event => {
-  //     const coords = _getCoordsObj(event.latLng);
-  //     this._handleClick(coords);
-  //   });
-  // }
-  //
-  // _handleMarkerClick(bench) {
-  //   this.props.router.push(`benches/${bench.id}`);
-  // }
-  //
-  // _handleClick(coords) {
-  //   this.props.router.push({
-  //     pathname: "benches/new",
-  //     query: coords
-  //   });
-  // }
+    this.map = new google.maps.Map(document.getElementById('single-biz-map'), _mapOptions);
+    this.MarkerManager = new MarkerManager(this.map);
+    this.MarkerManager.addMarker(this.props.business);
+  }
 
   render() {
     return (
