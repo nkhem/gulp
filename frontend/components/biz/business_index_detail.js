e8b2f6199bdeb95016f7b4b5fe9de15ed1c94e86
Nicole Hemenway <nkhemenway@gmail.com>
2017-05-01 14:50:18 -0700

    preliminary formatting for biz index list

--- frontend/components/biz/business_index_detail.js
+++ frontend/components/biz/business_index_detail.js
@@ -15,17 +15,14 @@ class BusinessIndexDetail extends React.Component {
   render() {
     const biz = this.props.business;
     return (
-      <div key={biz.id}>
-        <li>
-          <img src={`${biz.image_url}`} width={100} />
-          <h4 onClick={this.handleClick.bind(this)}>{biz.title}</h4>
-          <p>{biz.price}</p>
-          <p>{biz.phone}</p>
-          <p>{biz.address1}</p>
-          <p>{biz.address2}</p>
-        </li>
-        <br />
-      </div>
+      <li key={biz.id} className="biz-index-detail">
+        <img src={`${biz.image_url}`} width={100} />
+        <h3 onClick={this.handleClick.bind(this)}>{biz.title}</h3>
+        <p>{biz.price}</p>
+        <p>{biz.phone}</p>
+        <p>{biz.address1}</p>
+        <p>{biz.address2}</p>
+      </li>
     );
   }
 
