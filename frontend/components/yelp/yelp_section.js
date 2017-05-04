aa7ede3e6106018e49b7fcfa1ba6265a98b69eba
Nicole Hemenway <nkhemenway@gmail.com>
2017-05-04 13:21:52 -0700

    asdfasd

--- frontend/components/yelp/yelp_section.js
+++ frontend/components/yelp/yelp_section.js
@@ -4,7 +4,7 @@ import Stars from './stars';
 const YelpSection = (props) => {
   return (
     <div className='yelp-section'>
-    <a href={props.yelp_url} target="_blank">
+    <a href={props.yelpUrl} target="_blank">
       <img src="http://res.cloudinary.com/dx0y3rkfs/image/upload/v1493840751/Yelp_burst_positive_RGB_tmqdjp.png"></img>
     </a>
     <p>{props.rating}</p>
