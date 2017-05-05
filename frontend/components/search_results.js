d00d212fbf7cd94cecee1a4c601e483cc709edc6
Nicole Hemenway <nkhemenway@gmail.com>
2017-05-04 23:43:05 -0700

    make search map markers work better

--- frontend/components/search_results.js
+++ frontend/components/search_results.js
@@ -2,13 +2,10 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import BusinessIndex from './biz/business_index';
-import SearchMap from './map/search_map';
 
 const SearchResults = props => (
   <div className='search-results-container'>
     <BusinessIndex />
-    <SearchMap
-      businesses={props.businesses} />
   </div>
 );
 
