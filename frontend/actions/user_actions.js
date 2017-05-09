33b39c76da2b20a1231c048c91a0b3ec5b1e5a91
Nicole Hemenway <nkhemenway@gmail.com>
2017-05-08 22:37:09 -0700

    get user reviews to change in session state refresh on edit

--- frontend/actions/user_actions.js
+++ frontend/actions/user_actions.js
@@ -1,14 +1,19 @@
 import * as UserApiUtil from '../util/user_api_util';
 import * as SessionActions from '../actions/session_actions';
 
+export const RECEIVE_USER = "RECEIVE_USER";
 
 export const fetchUser = userId => dispatch => {
   return UserApiUtil.fetchUser(userId)
     .then(user => dispatch(receiveUser(user)));
 };
 
+export const receiveUser = user => ({
+  type: RECEIVE_USER,
+  user: user
+});
+
 export const refreshUser = userId => dispatch => {
-  console.log('refreshUser action');
   return UserApiUtil.refreshUser(userId)
-    .then(user => dispatch(receiveUser(user)));
+  .then(user => dispatch(SessionActions.receiveCurrentUser(user)));
 };
